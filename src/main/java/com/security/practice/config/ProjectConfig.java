db1f527699eb418abf456fa47fe0f6d0aec8ed2e
jaeseonNamgung <sunnamgung8@naver.com>
2023-03-03 13:51:50 +0900

    사용자 인증 설정

--- src/main/java/com/security/practice/config/ProjectConfig.java
+++ src/main/java/com/security/practice/config/ProjectConfig.java
@@ -2,15 +2,16 @@ package com.security.practice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig{
 
     @Bean
     public UserDetailsService userDetailsService(){
@@ -25,9 +26,25 @@ public class ProjectConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        
+
         // 암호에 암호화나 해시를 적용하지 않고 일반 텍스트 처럼 처리할 때 사용
         // 운영 단계에서는 절대 사용하면 안된다. (Deprecated 됨)
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    // SpringSecurity 5.4 이후 부터 WebSecurityConfigurerAdapter Deprecated 됨
+    // configurer -> SecurityFilterChain
+    // authorizeRequests 메서드도 Deprecated 됨
+    // authorizeRequests -> authorizeHttpRequest(()-> {});
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        // 모든 요청에 인증이 필요
+        http.authorizeHttpRequests(authz->authz.anyRequest().authenticated());
+
+        // 인즈 없이 요청
+        // http.authorizeHttpRequests(authz->authz.anyRequest().permitAll());
+        return http.build();
     }
 }
