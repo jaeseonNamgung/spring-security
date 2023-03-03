5ddb1b8230911c619e64ed374196903d6af56257
jaeseonNamgung <sunnamgung8@naver.com>
2023-03-03 10:24:20 +0900

    Initial Spring Boot

---
+++ src/test/java/ssia/ch2/ex1/Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ssia.ch2.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
