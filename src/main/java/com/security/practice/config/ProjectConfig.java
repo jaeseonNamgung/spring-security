06accc1b6565d1e0fcf2fc8a1ea7aaca28e8aeac
jaeseonNamgung <sunnamgung8@naver.com>
2023-03-03 15:40:18 +0900

    CustomAuthenticationProvider 사용법

--- src/main/java/com/security/practice/config/ProjectConfig.java
+++ src/main/java/com/security/practice/config/ProjectConfig.java
@@ -1,31 +1,39 @@
 package com.security.practice.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.function.Function;
-
+@RequiredArgsConstructor
 @Configuration
 public class ProjectConfig{
 
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+    private final CustomAuthenticationProvider authenticationProvider;
+    private AuthenticationManager authenticationManager;
 
-        UserDetails user = User.withUsername("jaeseon")
-                .password("0070")
-                .authorities("read")
-                .passwordEncoder((Function<String, String>) NoOpPasswordEncoder.getInstance())
-                .build();
-        return new InMemoryUserDetailsManager(user);
-            
+
+    @Bean
+    public CustomAuthenticationProvider authenticationProvider(){
+        return new CustomAuthenticationProvider();
     }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder  authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+        authenticationManager = authenticationManagerBuilder.build();
+
+        http.httpBasic();
+        http.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+        http.authenticationManager(authenticationManager);
+        return http.build();
+    }
+
+
+
 }
