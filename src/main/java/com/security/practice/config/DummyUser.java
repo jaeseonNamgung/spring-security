a60ee667826f741e51b05be6fa4e5e57de26985b
jaeseonNamgung <sunnamgung8@naver.com>
2023-03-14 15:58:19 +0900

    UserDetails 구현 방법

---
+++ src/main/java/com/security/practice/config/DummyUser.java
@@ -0,0 +1,57 @@
+package com.security.practice.config;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class DummyUser implements UserDetails {
+
+    private final String username;
+    private final String password;
+
+    public DummyUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // return List.of(()->"READ");
+        // or
+        return List.of(new SimpleGrantedAuthority("READ"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    // 모든 기능 활성화 -true
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
